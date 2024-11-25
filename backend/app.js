cfa857af142d0e4984fc062c01e709de8299a94a
IrakizaGaius <g.irakiza@alustudent.com>
2024-11-26 01:23:15 +0200

    changes

--- backend/app.js
+++ backend/app.js
@@ -33,5 +33,5 @@ app.use('/api/events', authenticate, authorize(['School Admin', 'Teacher']), eve
 app.use('/api/terms', authenticate, authorize(['School Admin', 'Teacher']), termRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
