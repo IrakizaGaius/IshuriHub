91054547fd4153351706c1f14c14279041992381
IrakizaGaius <g.irakiza@alustudent.com>
2024-11-25 23:30:19 +0200

    Increase timeout for deployment

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,20 +6,29 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      connectTimeout: 60000, // 60 seconds
+    },
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      connectTimeout: 60000, // 60 seconds
+    },
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      connectTimeout: 60000, // 60 seconds
+    },
   }
 };
