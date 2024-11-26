a91b5b4a05df1f3ef818ee8f98ccbf7c36860d3c
IrakizaGaius <g.irakiza@alustudent.com>
2024-11-26 12:01:08 +0200

    HashRouter workAround

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import IndexPage from './components/IndexPage';
 import Parents from './components/Parents';
